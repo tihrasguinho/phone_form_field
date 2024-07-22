360dca9990cf1bde22f584b3e87d5da61611192f
Tiago Alves <tiago.pessoal93@gmail.com>
2024-07-22 09:29:54 -0300

    validator adicionado para cada tipo de ddi

--- lib/src/phone_controller.dart
+++ lib/src/phone_controller.dart
@@ -8,4 +8,28 @@ class PhoneController extends MaskedTextController {
     super.beforeChange,
     super.cursorBehavior,
   });
+
+  String? maskValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'empty';
+    }
+
+    final pattern = mask.split('').map(
+      (l) {
+        if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(l)) {
+          return r'\d';
+        } else {
+          return '\\$l';
+        }
+      },
+    );
+
+    final regex = RegExp('^${pattern.join()}\$');
+
+    if (!regex.hasMatch(value)) {
+      return 'invalid';
+    } else {
+      return null;
+    }
+  }
 }
